e3534caaaac7dd059bcf2c48cc81ceb417e5f1a7
varsha <varshavsl.30@gmail.com>
2025-05-25 13:12:24 +0530

    updates

--- src/components/Definitions/Definitions.jsx
+++ src/components/Definitions/Definitions.jsx
@@ -1,16 +1,45 @@
 import "./Definitions.css";
 
-const Definitions = ({ word, category, meanings }) => {
+const Definitions = ({ word, category, LightTheme, meanings }) => {
   return (
     <div className="meanings">
+      {meanings[0] && word && category === "en" && (
+        <audio
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+          style={{ backgroundColor: "#ffff", borderRadius: 10 }}
+          controls
+        >
+          Your Browser doesn't support audio element.
+        </audio>
+      )}
+
       {word === " " ? (
         <span className="subTitle">Start by typing a word in Search</span>
       ) : (
         meanings.map((mean) =>
           mean.meanings.map((item) =>
-            item.definitions.map((def) => <div className="singleMean">
-
-            </div>)
+            item.definitions.map((def) => (
+              <div
+                className="singleMean"
+                style={{
+                  backgroundColor: LightTheme ? "#3b5360" : "white",
+                  color: LightTheme ? "white" : "black",
+                }}
+              >
+                <b>{def.definition}</b>
+                <hr style={{ backgroundColor: "black", width: "100%" }} />
+                {def.example && (
+                  <span>
+                    <b>Example :</b> {def.example}
+                  </span>
+                )}
+                {def.synonyms && (
+                  <span>
+                    <b>Synonyms :</b> {def.synonyms.map((s) => `${s}, `)}
+                  </span>
+                )}
+              </div>
+            ))
           )
         )
       )}
